f9dcb960d02c129f8b485a0c735b9222e0882653
Matti Salokangas <matti.salokangas@gmail.com>
2016-01-26 17:36:33 -0800

    Initial Commit

---
+++ app/scripts/directives/dead-cat.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moxieApp.directives:DeadCat
+ * @description
+ * # DeadCat
+ * Directive for the dead cat.  Meow.
+ */
+angular.module('moxieApp')
+  .directive('deadCat', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: "<div class='moxie-logo'><div class='moxie-logo__left-ear'></div><div class='moxie-logo__right-ear'></div><div class='moxie-logo__head'></div><div class='moxie-logo__eye'></div><div class='moxie-logo__body'></div><div class='moxie-logo__arms'></div><div class='moxie-logo__legs'></div><div class='moxie-logo__tail'></div></div>",
+      link: function(scope, element, attrs) {
+      }
+    }
+  });
